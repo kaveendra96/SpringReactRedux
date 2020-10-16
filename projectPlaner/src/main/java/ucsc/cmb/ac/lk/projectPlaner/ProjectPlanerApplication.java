41852572ec908ae55d15f8a0e50bed54509c61cf
nipun <kaveendra96@gmail.com>
2020-10-16 09:52:27 +0530

    project initialization

---
+++ projectPlaner/src/main/java/ucsc/cmb/ac/lk/projectPlaner/ProjectPlanerApplication.java
@@ -0,0 +1,13 @@
+package ucsc.cmb.ac.lk.projectPlaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPlanerApplication.class, args);
+	}
+
+}
---
+++ projectPlaner/src/test/java/ucsc/cmb/ac/lk/projectPlaner/ProjectPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package ucsc.cmb.ac.lk.projectPlaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

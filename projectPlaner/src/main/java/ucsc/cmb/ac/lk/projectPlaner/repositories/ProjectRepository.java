7c49c14f3b28dc601792c74cc54251648718bfe6
kaveendra96 <kaveendra96@gmail.com>
2020-10-23 21:27:23 +0530

    findbyprojectIdentifier

--- projectPlaner/src/main/java/ucsc/cmb/ac/lk/projectPlaner/repositories/ProjectRepository.java
+++ projectPlaner/src/main/java/ucsc/cmb/ac/lk/projectPlaner/repositories/ProjectRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import ucsc.cmb.ac.lk.projectPlaner.domain.Project;
 
 public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+    Project findByProjectIdentifier(String projectId);
+
     @Override
-    Iterable<Project> findAllById(Iterable<Long> iterable);
+    Iterable<Project> findAll();
 }
--- projectPlaner/src/main/java/ucsc/cmb/ac/lk/projectPlaner/services/ProjectService.java
+++ projectPlaner/src/main/java/ucsc/cmb/ac/lk/projectPlaner/services/ProjectService.java
@@ -3,6 +3,7 @@ package ucsc.cmb.ac.lk.projectPlaner.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ucsc.cmb.ac.lk.projectPlaner.domain.Project;
+import ucsc.cmb.ac.lk.projectPlaner.exceptions.ProjectIdException;
 import ucsc.cmb.ac.lk.projectPlaner.repositories.ProjectRepository;
 
 
@@ -11,6 +12,19 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch (Exception e){
+            throw new ProjectIdException("Project Id :" + project.getProjectIdentifier().toUpperCase()+ "  already Exists");
+        }
+    }
+    public Project findProjectByIdentifer(String projectId){
+        Project project= projectRepository.findByProjectIdentifier(projectId);
+        if(project == null)
+            throw new ProjectIdException("Project Id :" + projectId.toUpperCase()+ "  does not Exists");
+
+        return project;
     }
 }
